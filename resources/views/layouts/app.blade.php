3c3c39c439a0314a72a46e667b85fe398c61c47d
jhongmez7 <gmezjhon@gmail.com>
2021-02-15 18:16:34 -0500

    Added fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fontawesome-all.min.css') }}" rel="stylesheet">
+    
 </head>
 <body>
 
